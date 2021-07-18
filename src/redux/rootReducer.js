aa8601711d453bb84f0b9986f04993586a0c6f91
anik <anik@w3engineers.com>
2021-07-18 18:54:42 +0600

    # Improvement   - movie reducer removed from redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import genreReducer from './genre';
-import movieReducer from './movie';
 import watchListReducer from './watchList';
 import movieDetailReducer from './movieDetail';
 import viewListReducer from './viewList';
@@ -8,7 +7,6 @@ import viewListReducer from './viewList';
 // combine all reducers
 export default combineReducers({
   genre: genreReducer,
-  movie: movieReducer,
   watchList: watchListReducer,
   movieDetail: movieDetailReducer,
   viewList: viewListReducer,
